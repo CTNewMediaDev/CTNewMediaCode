e089c22689192bd451e3c71a7a9128f32d8774be
jianghaolin <jianghaolin312@gmail.com>
2016-03-11 18:02:16 +0800

    user模块添加

---
+++ user/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ 用户首页
+ */
+ini_set('display_errors', 1);
+require_once '../config.inc.php';
+
+//用户openid
+if(empty($_SESSION['openid'])){
+    $redirecturl = SITE_DOMAIN.'category.php';
+    $redirecturl .= '#'.time();
+    SystemTool::checkOpenid($db,'snsapi_userinfo',$redirecturl);
+}
+$title='个人资料';
+$pageidx = 'index';
+$user_info=Userinfo::getUserinfobyDb($db,$_SESSION['openid']);
+
+include 'template/index.html';
