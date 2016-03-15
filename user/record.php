679dbab305c84303c4e1613242284051abbe6d5b
jianghaolin <jianghaolin312@gmail.com>
2016-03-15 18:02:36 +0800

    user功能及界面

--- user/record.php
+++ user/record.php
@@ -11,6 +11,14 @@ if(empty($_SESSION['openid'])){
     $redirecturl .= '#'.time();
     SystemTool::checkOpenid($db,'snsapi_userinfo',$redirecturl);
 }
+
+if(!empty($_GET['action'])&&$_GET['action']=='more'){
+    $page=$_GET['page_now'];
+    $res=$db->fetch_all('select A.title,C.addtime,C.money as cmoney,A.money as amoney from clickcount as C left join articles as A on C.contentid=A.id where C.clickOpenid="'.$_SESSION['openid'].'"  limit '.(($page-1)*10).',10');
+    echo json_encode($res);
+    exit;
+}
+
 $title='赚钱记录';
 $pageidx = 'index';
 $records=ClickCount::getClickInfoByDb($db,$_SESSION['openid']);
