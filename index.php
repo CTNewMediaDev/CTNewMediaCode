43470d7530a87838ea080f1a8b8b299572d87145
hesh.liucuan <hesh.liucuan@gmail.com>
2016-04-27 18:06:29 +0800

    首页功能

--- index.php
+++ index.php
@@ -14,17 +14,57 @@ if(empty($_SESSION['openid'])){
 	SystemTool::checkOpenid($db,'snsapi_userinfo',$redirecturl);
 }
 
-//首页banner
-$sql = "select * from banners where position='首页' and status=1";
-$banners = $db->fetch_all($sql);
+//文章数据
+if(isset($_GET['catid'])){
+	$catid = intval($_GET['catid']);
+}
+if(isset($_GET['order'])){
+	$order = $_GET['order'];
+	if(!in_array($order,array('visitcount','sharenum','collectnum','money','leftmoney'))){
+		$order = '';
+	}
+}else{
+	$order = '';
+}
+
+$sql = "select * from articles where status=1";
+if(!empty($catid))
+	$sql .= " and categoryid=".$catid;
+if(empty($order))
+	$sql .= " order by id desc limit 10";
+else
+	$sql .= " order by ".$order." desc,id desc limit 10";
+$articles = $db->fetch_all($sql);
+
+foreach($articles as $key=>$item){
+	$storeaddr = json_decode($item['storeaddr'],true);
+	$articles[$key]['storedist'] = $storeaddr['district'];
+}
+
 
 //分类数据
 $categories = SystemTool::getAllCategory($db);
 
-//默认数据
-$sql = "select * from articles where status=1 order by id desc limit 10";
-$articles = $db->fetch_all($sql);
+//banner图
+if(!empty($catid)){
+	for($i=0;$i<count($categories);$i++){
+		if($categories[$i]['id']==$catid){
+			$catname = $categories[$i]['name'];
+			break;
+		}
+	}
+	$sql = "select * from banners where position='".$catname."' and status=1";
+	$banners = $db->fetch_all($sql);
+}
+if(empty($banners)){
+	$sql = "select * from banners where position='首页' and status=1";
+	$banners = $db->fetch_all($sql);
+}
 
+//排序链接地址
+$orderlink = 'index.php?';
+if(!empty($catid))
+	$orderlink .= 'catid='.$catid."&";
 
 $pageidx = 'index';
 
