fbf05c759deaa437ab3f1cc9b84066a0a31cd69a
jianghaolin <jianghaolin312@gmail.com>
2016-05-06 19:15:40 +0800

    收藏修改

--- user/collection.php
+++ user/collection.php
@@ -13,12 +13,20 @@ if(empty($_SESSION['openid'])){
 }
 $title='我的收藏';
 $pageidx = 'index';
+
+$pagesize=2;
+$startindex = 0;
+
 $collections=Collection::getCollection($db,0,10);
 if($collections){
     for($i=0;$i<count($collections);$i++){
         $contentInfo=ContentClass::getArticle($db,$collections[$i]['contentid']);
         if($contentInfo) {
             $collections[$i] = array_merge($collections[$i], $contentInfo);
+            $storeaddr=json_decode($collections[$i]['storeaddr'],true);
+            $collections[$i]['district']=$storeaddr['district'];
+            $collections[$i]['starttime']=date('Y-m-d',$collections[$i]['starttime']);
+            $collections[$i]['endtime']=date('Y-m-d',$collections[$i]['endtime']);
         }else{
             unset($collections[$i]);
         }
